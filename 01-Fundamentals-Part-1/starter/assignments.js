2664bdf8c159b37448ae4d62659fb6a8a2668906
Jacson Barbosa <barbosajacson.jb@gmail.com>
2026-06-18 16:01:19 -0300

    fix: Values and variables

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,8 @@
+let country = "Brazil";
+let continent = "South America";
+let population = 213000000;
+
+console.log("Country: " + country);
+console.log("Continent: " + continent);
+console.log("Population: " + population);
+
