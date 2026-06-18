0051f50c6476ef9919babba3a2abf13722f4673d
Jacson Barbosa <barbosajacson.jb@gmail.com>
2026-06-18 15:51:05 -0300

    Values and variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,16 @@
 let Js = "Amazing";
-    if (Js === "Amazing") {
-      alert("JavaScript is FUN!😀");
-    }
+   console.log(40+50-20);
+console.log("Jackson");
+let firstName = "Jackson";
+console.log("First Name: " + firstName);
+
+
+let myCurrentJob = "Student";
+let myFirstJob = "Technician";
+let myFutureJob = "Software Engineer";
+
+console.log("First Job: " + myFirstJob);
+console.log("Current Job: " + myCurrentJob);
+console.log("Future Job: " + myFutureJob);
+
+
