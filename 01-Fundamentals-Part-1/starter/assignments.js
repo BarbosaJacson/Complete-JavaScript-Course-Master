1738da81d5acf6d0c0259dde824cead3e8d2b22c
Jacson Barbosa <barbosajacson.jb@gmail.com>
2026-06-21 12:23:29 -0300

    Solve coding Challenge #2 Mark's BMI is higher than John's!

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -42,11 +42,11 @@ let BMIJohn = massJohn / (heightJohn*heightJohn);
 console.log("BMI of Mark is"+ " "+ BMIMark);
 console.log("BMI of John is"+ " "+ BMIJohn);
 
-//TEST DATA 2 
-massMark = 95;
-massJohn = 85;
-heightJohn = 1.76;
-heightMark = 1.88;
+//TEST DATA 2 Inibido para não sobrescrever os dados do teste 1, caso queira testar o teste 2, basta comentar os dados do teste 1 e descomentar os dados do teste 2
+//massMark = 95;
+//massJohn = 85;
+//heightJohn = 1.76;
+//heightMark = 1.88;
 
 BMIMark = massMark / (heightMark * heightMark);
 BMIJohn = massJohn / (heightJohn*heightJohn);
@@ -57,3 +57,36 @@ console.log("BMI of John is"+ " "+ BMIJohn);
 let markHigherBMI = BMIMark > BMIJohn;
 
 console.log(markHigherBMI);
+/*CHALLENGE #2
+
+
+Use the BMI example from Challenge #1, and the code you already wrote, and improve it:
+
+1. Print a nice output to the console, telling the user who has the higher BMI. The message can be either:
+"Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!".
+
+2. Modify the outputs above to use template literals to include the BMI values in the outputs.
+
+Example: "Mark's BMI (28.3) is higher than John's (23.9)!" or "John's BMI (29.1) is higher than Mark's (27)!".
+
+Note: Don't round the BMI values. Leave them as they are.
+
+
+👉 OPTIONAL: You can watch my solution in video format in the next lecture
+
+
+IMPORTANT: The ** operator is not supported in this editor. Please make sure to use exactly this formula mass / (height * height), and not this one mass / (height ** 2).
+
+*/
+
+
+/* Write your code below. Good luck! 🙂 */
+
+if(BMIMark > BMIJohn){ console.log("Mark's BMI is higher than John's!")
+} else { console.log("John's BMI is higher than Mark's!")}
+
+BMIMarkNew = 28.3;
+BMIJohnNew = 29.1;
+
+if(BMIMark > BMIJohn){ console.log("Mark's BMI " + (BMIMarkNew) + " is higher than John's!")
+} else { console.log("John's BMI " + (BMIJohnNew) + " is higher than Mark's!")}
