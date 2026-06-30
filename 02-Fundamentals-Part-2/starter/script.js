5fc0edec6e737ad3d976452711ae6c60ab80e3ab
Jacson Barbosa <barbosajacson.jb@gmail.com>
2026-06-30 11:20:05 -0300

    feat(js): Arrow Function with multiple parameters and multiple lines of code, with the return statement and curly braces.

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -28,12 +28,25 @@ console.log(appleOrangeJuice);
 
 //Function Declaration
 function calcAge1(birthYear) {
-  return 2050 - birthYear;
+  return 2040 - birthYear;
 }
 const age1 = calcAge1(1978);
 //Function Expression
 const calcAge2 = function (birthYear) {
-  return 2050 - birthYear;
+  return 2040 - birthYear;
 };
 const age2 = calcAge2(1978);
 console.log(age1, age2);
+
+//Arrow Function
+const calcAge3 = birthYear => 2040 - birthYear; //Function with one parameter and one line of code, without the return statement and curly braces.
+const age3 = calcAge3(1978);
+console.log(age3);
+//Arrow Function with multiple parameters and multiple lines of code, with the return statement and curly braces.
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2026 - birthYear;
+  const retirement = 65 - age;
+  return `${firstName} retires in ${retirement} years.`;
+}
+console.log(yearsUntilRetirement(1978, 'Jacson'));
